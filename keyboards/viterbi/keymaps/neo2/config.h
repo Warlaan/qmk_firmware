fb2237ec1b659d2855666ed5f060f378cede1c0e
Lars Kokemohr <lars@lkokemohr.de>
2019-01-28 15:17:17 +0100

    added a mouse layer

--- keyboards/viterbi/keymaps/neo2/config.h
+++ keyboards/viterbi/keymaps/neo2/config.h
@@ -38,4 +38,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
 
+#define MOUSEKEY_DELAY        100
+#define MOUSEKEY_INTERVAL     20
+#define MOUSEKEY_MAX_SPEED    4
+#define MOUSEKEY_TIME_TO_MAX  4
+
 #endif
