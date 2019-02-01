1d378d4be8facb7e31f472f2be3e8f352929fffd
Lars Kokemohr <lars@lkokemohr.de>
2019-02-01 11:51:46 +0100

    Added keys for § and €, for windows media and for powering off lowered the auto shift timeout

--- keyboards/viterbi/keymaps/neo2/config.h
+++ keyboards/viterbi/keymaps/neo2/config.h
@@ -43,6 +43,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MOUSEKEY_MAX_SPEED    4
 #define MOUSEKEY_TIME_TO_MAX  4
 
-#define AUTO_SHIFT_TIMEOUT 70
+#define AUTO_SHIFT_TIMEOUT 50
+//#define AUTO_SHIFT_SETUP
 
 #endif
